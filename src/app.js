37344874eb91215286fae841ab502d7cc5e2c37f
Mu7aMMaD <69051329+iiMu7aMMaD@users.noreply.github.com>
2022-08-13 05:06:56 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -53,7 +53,83 @@ function loaded() {
 
 var x = 0;
 
+import './assets/styles/app.css';
+import logo from './assets/images/avatar.png';
+import Wave from 'react-wavify'
+
+function App() {
+  return (
+    <div className="App">
+      <Wave className="App-canvas" fill='#fff'
+        paused={false}
+        options={{
+          height: 80,
+          amplitude: 40,
+          speed: 0.05,
+          points: 4
+        }}
+      ></Wave>
+      <header className="App-header">
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div>
+          <div>
+            <h1>
+              <span>Muhammad</span> <span >H</span>
+            </h1>
+            <h3>
+              <span>ii_mu7ammad</span>
+            </h3>
+          </div>
+          <h4>
+            <span className="span">¨Don’t give up on your dreams, or your dreams will give up on you.¨</span>
+          </h4>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+// title 
+if (document.addEventListener) {
+  document.addEventListener("DOMContentLoaded", function () {
+    loaded();
+  });
+} else if (document.attachEvent) {
+  document.attachEvent("onreadystatechange", function () {
+    loaded();
+  });
+}
+
+function loaded() {
+  setInterval(loop, 1500);
+}
+
+var x = 0;
+
 var titleText = [
+  "Muhammad",
+  "Muhammad ",
+  "Muhammad |",
+  "Muhammad | ",
+  "Muhammad | I",
+  "Muhammad | Im",
+  "Muhammad | Ima",
+  "Muhammad | Imag",
+  "Muhammad | Imagi",
+  "Muhammad | Imagin",
+  "Muhammad | Imagine",
+  "Muhammad | Imagine ",
+  "Muhammad | Imagine a",
+  "Muhammad | Imagine a ",
+  "Muhammad | Imagine a p",
+  "Muhammad | Imagine a pl",
+  "Muhammad | Imagine a pla",
+  "Muhammad | Imagine a plac",
+  "Muhammad | Imagine a place",
+  "Muhammad | Imagine a place.",
+  "Muhammad | Imagine a place..",
   "Muhammad | Imagine a place...",
   "Muhammad | Imagine a place..",
   "Muhammad | Imagine a place.",
@@ -73,8 +149,18 @@ var titleText = [
   "Muhammad | Im",
   "Muhammad | I",
   "Muhammad | ",
-  "Muhammad |",
+  "Muhammad ",
+  "Muhammad",
 ];
+
+function loop() {
+  document.getElementsByTagName("title")[0].innerHTML =
+    titleText[x++ % titleText.length];
+}
+// titile
+
+export default App;
+
 
 function loop() {
   document.getElementsByTagName("title")[0].innerHTML =
